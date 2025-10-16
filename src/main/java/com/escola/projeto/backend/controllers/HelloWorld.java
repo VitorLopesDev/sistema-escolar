f8f14769467272ef70223823e8203f76b53fc1a9
vitorlopesdev <dev.vitorlopes@gmail.com>
2025-10-15 22:28:31 -0300

    application e docker compose

--- src/main/java/com/escola/projeto/backend/controllers/HelloWorld.java
+++ src/main/java/com/escola/projeto/backend/controllers/HelloWorld.java
@@ -1,4 +1,4 @@
-package com.escola.projeto.backend.controllers; // mesmo pacote da aplicação
+package com.escola.projeto.backend.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +8,6 @@ public class HelloWorld {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello world";
+        return "Hello";
     }
 }
